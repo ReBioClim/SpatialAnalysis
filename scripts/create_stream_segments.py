3a4d7161eb5f93bb679415699cff0f2eac599944
yehanwu <yehanwu1@gmail.com>
2025-07-14 15:48:07 +0200

    update

--- scripts/create_stream_segments.py
+++ scripts/create_stream_segments.py
@@ -1,10 +1,14 @@
+from shapely.strtree import STRtree
 import geopandas as gpd
-from shapely.geometry import LineString, Point, MultiPoint
+from shapely.geometry import LineString, Point, MultiPoint, MultiLineString, MultiPolygon, Polygon
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-from shapely.ops import linemerge, unary_union, split, snap
+from shapely.ops import linemerge, unary_union, split, snap, substring
 from tqdm import tqdm
+from scipy.spatial import cKDTree
+from shapely.ops import unary_union
+
 
 
 # create stream segments
@@ -141,104 +145,4 @@ segment_with_attrs.drop(columns=["index_right"], inplace=True)
 segment_with_attrs.to_file("data/stream_geometry/stream_segments_100m_with_attrs.gpkg",
                            layer="segments_100m_with_attrs", driver="GPKG")
 
-
-
-# 250708
-# # create 100m buffer 
-stream100 = gpd.read_file("data/stream_segments/stream_segments_100m_with_attrs.gpkg",
-                           layer="segments_100m_with_attrs", driver="GPKG")
-
-
-print(len(stream100))
-print(stream100.crs)
-print(stream100.columns.unique())
-
-# stream100 create a column of ID, from 1
-stream100["segment_id"] = range(1, len(stream100)+1)
-
-
-
-# a good way is to first multistringline all stream100, create overall bufferzone, 
-# and then cut the buffer with cutting line vertical to the stream segment direction
-merged_line = linemerge(unary_union(stream100.geometry))
-buffer_width = 50 # single side 50m  
-buffer_geom = merged_line.buffer(buffer_width)
-buffer_zone = gpd.GeoDataFrame(geometry=[buffer_geom], crs=stream100.crs)
-buffer_polygon = buffer_zone.iloc[0].geometry
-
-# create vertical cutting line
-def make_cutline(line, buffer_width=100, scale=2.0):
-    coords = list(line.coords)
-    if len(coords) < 2:
-        return None
-
-    p0 = np.array(coords[0])  # start point
-    p1 = np.array(coords[-1])  # end point
-
-    direction = p1 - p0
-    direction = direction / np.linalg.norm(direction)
-    normal = np.array([-direction[1], direction[0]])  # vertical
-    normal = normal / np.linalg.norm(normal)
-
-    length = buffer_width * scale
-    offset = normal * (length / 2)
-
-    p_left = p0 + offset
-    p_right = p0 - offset
-
-    return LineString([tuple(p_left), tuple(p_right)])
-
-cutlines = stream100.copy()
-cutlines["geometry"] = cutlines["geometry"].apply(lambda g: make_cutline(g, buffer_width=100))
-cutlines = cutlines[cutlines["geometry"].notnull()]
-print(len(cutlines))
-
-cutlines.to_file("data/stream_segments/cutlines.gpkg", driver="GPKG")
-
-# split buffer
-
-all_parts = [buffer_polygon]
-for i, line in tqdm(enumerate(cutlines.geometry), total=len(cutlines)): # progress bar # it takes time, 15min
-    updated_parts = []
-
-    for geom in all_parts:
-        if not line.intersects(geom):
-            updated_parts.append(geom)
-            continue
-
-        # cut
-        try:
-            result = split(geom, line)
-            updated_parts.extend(result.geoms)
-        except Exception as e:
-            print(f"Line {i} split failed: {e}")
-            updated_parts.append(geom)
-
-    # updated parts for next cutting
-    all_parts = updated_parts
-
-print(len(all_parts))
-
-buffer_segments = gpd.GeoDataFrame(geometry=all_parts, crs=buffer_zone.crs)
-
-
-
-if "segment_id" in stream100.columns:
-    buffer_segments = gpd.sjoin(buffer_segments, stream100[["segment_id", "geometry"]],
-                                how="left", predicate="intersects")
-
-buffer_segments.to_file("data/stream_segments/stream_buffer100_segments.gpkg", driver="GPKG")
-
-# filter out extreme value, cleaning
-buffer_segments = gpd.read_file("data/stream_segments/stream_buffer100_segments.gpkg", driver="GPKG")
-buffer_segments["area"] = buffer_segments.geometry.area / 10000 # unit is ha
-
-buffer_segments_clean = buffer_segments[(buffer_segments["area"] >= 0.8) & (buffer_segments["area"] <= 1.2)].copy()
-
-buffer_segments_clean["area"].hist() # after checking the histogram, and the geometry on map, 0.8-1.2 is a reasonable range
-plt.show()
-
-print(buffer_segments.crs)
-
-buffer_segments_clean.to_file("data/stream_segments/stream_buffer100_cleaned.gpkg", driver="GPKG")
 
